1ebbeef4528f4958b5efa9ac4de2af2831068951
Eyob Kibret <64076953+Eyobkibret15@users.noreply.github.com>
2021-09-03 11:53:58 +0200

    Update client.py

--- client/client.py
+++ client/client.py
@@ -23,7 +23,7 @@
 # fp.close()
 
 import requests
-r = requests.get("http://127.0.0.1:1111/api/")
+r = requests.get("https://superuser.com/questions/938093/nano-alternative-for-windows-powershell")
 print("new 11111111111111111111111111111"
       "ftghfh"
       "fghgfhgjgj"
