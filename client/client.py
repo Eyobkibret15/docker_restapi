1bb1c45c56f6ae8714a537882c01b3a10fb08fbc
Eyob Kibret <64076953+Eyobkibret15@users.noreply.github.com>
2021-09-03 11:56:12 +0200

    Update client.py

--- client/client.py
+++ client/client.py
@@ -23,7 +23,7 @@
 # fp.close()
 
 import requests
-r = requests.get("https://superuser.com/questions/938093/nano-alternative-for-windows-powershell")
+r = requests.get("http://192.168.1.40/admin/")
 print("new 11111111111111111111111111111"
       "ftghfh"
       "fghgfhgjgj"
